977ba0f5305fad64331e5915b03df82a0945abd9
Topurayhan554 <topurayhantipu@gmail.com>
2026-03-10 11:07:00 +0600

    done

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:3000",
-  "https://job-portal-client-sigma-azure.vercel.app",
+  "https://kaajkhojo-sigma-azure.vercel.app",
   process.env.CLIENT_URL,
 ].filter(Boolean);
 
